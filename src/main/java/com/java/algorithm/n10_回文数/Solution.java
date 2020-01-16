c82115937bd7122562b6826d6305ca83e1c6329e
wb-wzw530056 <wb-wzw530056@autonavi.com>
2020-01-16 16:50:55 +0800

    回文数

---
+++ src/main/java/com/java/algorithm/n10_回文数/Solution.java
@@ -0,0 +1,29 @@
+package com.java.algorithm.n10_回文数;
+
+public class Solution {
+    public static boolean isPalindrome(int x) {
+        if(x<0){
+            return false;
+        }else if(x==0){
+            return true;
+        }else{
+            int num = 0;
+            int y = x;
+            while (x!=0){
+                int pop = x%10;
+                num = num*10 + pop;
+                x = x/10;
+            }
+            if(num==y){
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(isPalindrome(10));
+    }
+
+
+}
